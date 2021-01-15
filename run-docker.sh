3fd1876a24c822721602b8b52bd7f93919b7da7b
themioa <mioawoj@gmail.com>
2021-01-15 13:15:03 +0100

    Cleaning repo

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-docker build -t safeapp:latest .
+docker build --no-cache -t safeapp:latest .
 docker run --name safeapp -d -p 8000:5000 --rm safeapp:latest
+echo Aplikacja działa pod adresem https://localhost:8000/
